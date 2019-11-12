ee54e9b7424706320727118e4166839a1cf50c1a
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2019-11-12 15:24:49 +0200

    Docker postgres

--- scripts.sh
+++ scripts.sh
@@ -6,3 +6,6 @@ GRADLE_OPTS="-Xmx4g" snyk test --all-sub-projects
 
 # Redirect stdout and err
 cmd > log.out 2> log.err
+
+# Docker run postgres
+docker run -d -e POSTGRES_PASSWORD= -e POSTGRES_USER= -e POSTGRES_DB= -p 5432:5432 postgres
