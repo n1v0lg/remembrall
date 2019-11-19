cbdafc7fa71074e85700d03b0259441cbf365518
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2019-11-19 14:13:22 +0100

    Git diffs

--- scripts.sh
+++ scripts.sh
@@ -9,3 +9,6 @@ cmd > log.out 2> log.err
 
 # Docker run postgres
 docker run -d -e POSTGRES_PASSWORD= -e POSTGRES_USER= -e POSTGRES_DB= -p 5432:5432 postgres
+
+# Git diffs (e.g., two commits back on specific file)
+git diff HEAD~2 HEAD -- main.c
