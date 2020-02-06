2f266801544d1c405d239faf68529cc2bc2b7b23
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2020-02-06 14:42:22 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Snyk with larger heap
-GRADLE_OPTS="-Xmx4g" snyk test --all-sub-projects
+GRADLE_OPTS="-Xmx4g" snyk test --all-sub-projects --org=pleo
 
 # Redirect stdout and err
 cmd > log.out 2> log.err
